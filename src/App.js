19852f4aadf8f2561a5aedb2e359520468705315
xxzeeeeeexx <zayannazeenath@gmail.com>
2025-07-07 22:14:40 +0400

    Portfolio

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ function App() {
   return (
     <div className="container">
       <div className="text-container">  
-        <h1 className="title">Portfolio</h1>
-        <h1 className="title">under maintenance.</h1>
+        <h1 className="title">Under</h1>
+        <h1 className="title">maintenance.</h1>
         <p className="description">
           Good things take time. Check back soon to see what I've been working on!
         </p>
